3e3478f7ff4a69a9b196ffd19b44ee8117251b10
ABDIRAUF <user@Abdirauf.ABDIRAUF>
2025-03-05 14:06:42 +0300

    created

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -21,10 +21,12 @@
         @foreach ($events as $event)
         <div class="bg-white shadow-lg rounded-lg overflow-hidden p-4">
             <div class="text-center">
+            <div>
+                <img src="{{asset('storage/'.$event->image)}}" alt="image">
+                </div>
                 <p class="text-lg font-bold">ID: {{ $event->id }}</p>
                 <h3 class="text-gray-600 text-xl">{{ $event->title }}</h3>
                 <p class="text-gray-500">{{ $event->location }}</p>
-                <p class="text-gray-500">{{ $event->image }}</p>
                 <div class="mt-3 space-x-2">
                     <a href="{{ route('events.show', $event->id) }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">View Details</a>
                     <a href="{{ route('events.edit', $event->id) }}" class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-700">Edit</a>
